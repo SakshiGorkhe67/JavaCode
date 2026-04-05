9442a8097ef161de248b0abe1287b7f5c4397507
Sakshi <sakshigorkhe67@gmail.com>
2026-04-06 00:47:33 +0530

    removing the autogenerated lines

--- src/Arrays/LargestArrayElement.java
+++ src/Arrays/LargestArrayElement.java
@@ -3,7 +3,6 @@ package Arrays;
 public class LargestArrayElement {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int array[]=new int[]{10,5,15,3,7};
 		int max=Integer.MIN_VALUE;
 		for(int i:array) {
