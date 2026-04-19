5f460c9526aa5ca1ce0635efe53e0798af6216ce
Sakshi <sakshigorkhe67@gmail.com>
2026-04-20 01:49:45 +0530

    Hashset

--- src/Java8/streamAPI/EvenStreams.java
+++ src/Java8/streamAPI/EvenStreams.java
@@ -39,7 +39,56 @@ public class EvenStreams {
 		System.out.println("Descentding Sorted");
 		list.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
 
+		// 5. Find maximun number
 
+		int max=list.stream().max(Integer::compare).get();
+		System.out.println("Maximum number is: "+max);
+
+		// 6. Count Element
+
+		long count=list.stream().count();
+		System.out.println("Count is :"+count);
+
+		// 7. Convert list of string to uppercase
+		List<String> names=Arrays.asList("Sakshi","Gorkhe");
+		names.stream().map(String::toUpperCase).forEach(System.out::println);
+
+		// 8. Find First Element
+
+	    Optional<Integer> n=list.stream().findFirst();
+		System.out.println("First value:"+n);
+
+		list.stream().findFirst().ifPresent(System.out::println);
+
+
+		// 9. Sum of all elements
+
+		int sum=list.stream().reduce(0,(a,b)->a+b);
+		System.out.println("sum is:"+sum);
+
+
+		// 10. Filter string starting with S
+
+		names.stream().filter(name->name.startsWith("S")).forEach(System.out::println);
+
+       //11. Check if any number is greater than 10
+
+		boolean bool=list.stream().anyMatch(x->x>10);
+		System.out.println(bool);
+
+        // 12. Check if all numbers are positive
+
+		 boolean result=list.stream().allMatch(y->y>0);
+		System.out.println(result);
+
+
+		//13 Skip first 3 element
+
+		list.stream().skip(3).forEach(System.out::println);
+
+		// 14 .Get first 3 numbers
+
+		list.stream().limit(3).forEach(System.out::println);
 
 
 
