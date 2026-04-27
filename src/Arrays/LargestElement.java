1bd5a74b3e52173cb063313465271a41fcf5f6e8
Sakshi <sakshigorkhe67@gmail.com>
2026-04-27 22:23:38 +0530

    Streams Basic

---
+++ src/Arrays/LargestElement.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class LargestElement {
+    public static void main(String[] args) {
+        int arr[]={12,13,14,15,34,10};
+        int max=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(max<arr[i])
+            {
+                max=arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
