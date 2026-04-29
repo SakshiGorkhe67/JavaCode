3fc91af428d02b90b9ce6eeef3d9269624ede0d0
Sakshi <sakshigorkhe67@gmail.com>
2026-04-30 01:35:08 +0530

    Array Basic

---
+++ src/Arrays/HalfAcDec.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class HalfAcDec {
+    public static void main(String[] args) {
+        int arr[] = {4, 1, 2, 3, 4, 5};
+        int n = arr.length;
+     Arrays.sort(arr);
+
+     int start=n/2;
+     int end=n-1;
+
+     while(start!=end){
+         int temp = arr[start];
+         arr[start] = arr[end];
+         arr[end] = temp;
+         start++;
+         end--;
+     }
+        System.out.println(Arrays.toString(arr));
+    }
+}
